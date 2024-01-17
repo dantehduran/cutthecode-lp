7a2ff3d4b5bcca2a9de0d62c265bdf6446d8f03e
dante huillca duran <dantehduran@gmail.com>
2024-01-16 19:20:05 -0500

    product card: zoom effect on image

--- components/projects/Card.vue
+++ components/projects/Card.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="w-80 flex-shrink-0 flex flex-col gap-6 cursor-pointer">
-    <div class="w-full aspect-[3/4] rounded-2xl">
+  <div
+    class="group relative w-[22rem] flex-shrink-0 flex flex-col gap-6 cursor-pointer"
+  >
+    <div class="aspect-[3/4] overflow-hidden max-w-[22rem] rounded-2xl">
       <img
         :src="'/images' + img"
         :alt="client"
-        class="object-cover aspect-[3/4] rounded-2xl"
+        class="object-cover group-hover:scale-105 transition duration-300"
       />
     </div>
     <div class="flex flex-col gap-1">
@@ -12,6 +14,18 @@
         {{ desc }}
       </h3>
       <div class="opacity-60 font-semibold leading-[140%]">{{ client }}</div>
+    </div>
+    <div
+      v-show="case"
+      class="absolute top-0 -right-[1px] pl-4 pb-4 bg-muted rounded-bl-2xl flex justify-end items-start"
+    >
+      <div
+        class="text-sm font-semibold rounded-full flex items-center py-1 px-2 bg-background text-foreground"
+      >
+        Case
+      </div>
+      <Curve class="absolute text-muted -left-3.5 top-0 rotate-90" />
+      <Curve class="absolute text-muted -bottom-3.5 right-0 rotate-90" />
     </div>
   </div>
 </template>
